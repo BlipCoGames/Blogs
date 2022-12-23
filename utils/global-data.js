d9e6074b205475ff1ed3bfa735ed3800139aa68c
BlipCoGames <85733843+BlipCoGames@users.noreply.github.com>
2022-12-23 23:16:22 +0300

    Update global-data.js

--- utils/global-data.js
+++ utils/global-data.js
@@ -1,13 +1,13 @@
 export const getGlobalData = () => {
   const name = process.env.BLOG_NAME
     ? decodeURI(process.env.BLOG_NAME)
-    : 'Jay Doe';
+    : 'О Алине';
   const blogTitle = process.env.BLOG_TITLE
     ? decodeURI(process.env.BLOG_TITLE)
-    : 'Next.js Blog Theme';
+    : 'Автор Блога Валерий М.';
   const footerText = process.env.BLOG_FOOTER_TEXT
     ? decodeURI(process.env.BLOG_FOOTER_TEXT)
-    : 'All rights reserved.';
+    : 'Все права защищены и Алина тоже.';
 
   return {
     name,
